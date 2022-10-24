0f284227876a0816568ededafa21f823e406a107
Yumemira <surrenderyumi@mail.ru>
2022-10-24 11:05:45 +0300

    Login schema added too

--- client/src/views/MainMenuFrame.js
+++ client/src/views/MainMenuFrame.js
@@ -1,5 +1,8 @@
 import logo from '../logo.svg';
 import './MainMenuStyle.css';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function MainMenuElements()
 {
@@ -29,11 +32,41 @@ function MainMenuElements()
   elems[2] = (
     <div id="userProfile">
       <p class="inlineText">{"<<"}</p>
-      <button class="inlineText" id="login">Login</button>
+      <button class="inlineText" id="login" onClick="loginInstantiate()">Login</button>
     </div>
   ); 
 
   return elems;
 }
 
+function loginInstantiate()
+{
+
+  root.render(<loginElement />);
+}
+
 export default MainMenuElements;
+
+class loginElement extends React.Component
+{
+  render() {
+    return (
+      <section id="loginMenu">
+        <form id="logregField">
+          <fieldset>
+            <legend>Register:</legend>
+            <label for="email">email: </label>
+            <input type="email" name="email"></input>
+            <label for="username">name: </label>
+            <input type="text" name="username"></input>
+            <label for="password">password: </label>
+            <input type="password" name="password"></input>
+            <label for="repeatPassword">repeat: </label>
+            <input type="password" name="repeatPassword"></input>
+            <button name="submitForm" value="Register"></button>
+          </fieldset>
+        </form>
+      </section>
+    );
+  }
+}
