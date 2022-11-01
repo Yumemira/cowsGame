d2e5407dd7045127c5388638d7bdd0edf70e9365
Yumemira <surrenderyumi@mail.ru>
2022-11-01 21:00:38 +0300

    Toggling among register and login

--- client/src/views/MainMenuFrame.js
+++ client/src/views/MainMenuFrame.js
@@ -6,7 +6,7 @@ import classnames from "classnames";
 
 function MainMenuElements()
 {
-  const elems = [4];
+  const elems = [3];
   //TODO: container main with contents, container upperFrame, top-right account button
   // upper frame with buttons that represents play, forum and about links to go;
   
@@ -44,10 +44,15 @@ function toggling()
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: false};
+    this.state = {
+      isToggleOn: false,
+      isRegisterOn: true,
+      toggleButtonText: "Login"
+    };
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
+    this.changeFieldset = this.changeFieldset.bind(this);
   }
 
   handleClick = () => {
@@ -56,32 +61,85 @@ class Toggle extends React.Component {
     }));
   }
 
+  changeFieldset = () => {
+    let texting;
+    if(this.state.isRegisterOn)
+    {
+      texting = "Register"
+    }
+    else
+    {
+      texting = "Login"
+    }
+
+    this.setState(prevState => ({
+      isRegisterOn: !prevState.isRegisterOn,
+      toggleButtonText: texting
+    }));
+  }
+
   render() {
+    let fieldSet;
+
+    if(this.state.isRegisterOn)
+    {
+      fieldSet = <RegisterField />;
+    }
+    else
+    {
+      fieldSet = <LoginField />;
+    }
     return (
       <div>
         <div id="userProfile">
           <p className="inlineText">{"<<"}</p>
-          <button className="inlineText" id="login" onClick={this.handleClick}>Login</button>
+          <button className="inlineText" id="login" onClick={this.handleClick}>login</button>
         </div>
-        <section id="loginMenu" className={classnames("login", {
-            "login--hidden": !this.state.isToggleOn
+        <section id="loginMenu" className={classnames("--visibled", {
+            "--hidden": !this.state.isToggleOn
           })}>
           <form id="logregField">
-            <fieldset>
-              <legend>Register:</legend>
-              <label htmlFor="email">email: </label>
-              <input type="email" name="email"></input><br/>
-              <label htmlFor="username">name: </label>
-              <input type="text" name="username"></input><br/>
-              <label htmlFor="password">password: </label>
-              <input type="password" name="password"></input><br/>
-              <label htmlFor="repeatPassword">repeat: </label>
-              <input type="password" name="repeatPassword"></input><br/>
-              <button name="submitForm">Register</button>
-            </fieldset>
+            <input type='button' value={this.state.toggleButtonText} onClick={this.changeFieldset}></input>
+            {fieldSet}
           </form>
         </section>
       </div>
+    );
+  }
+}
+
+class RegisterField extends React.Component
+{
+  render() {
+    return(
+      <fieldset>
+        <legend>Register:</legend>
+        <label htmlFor="email">email: </label>
+        <input type="email" name="email"></input><br/>
+        <label htmlFor="username">name: </label>
+        <input type="text" name="username"></input><br/>
+        <label htmlFor="password">password: </label>
+        <input type="password" name="password"></input><br/>
+        <label htmlFor="repeatPassword">repeat: </label>
+        <input type="password" name="repeatPassword"></input><br/>
+        <button name="submitForm" className="submitForm">Register</button>
+      </fieldset>
+    );
+  }
+}
+
+class LoginField extends React.Component
+{
+  render() {
+    return(
+      <fieldset>
+        <legend>login</legend>
+        <label htmlFor="email">email: </label>
+        <input type="email" name="email"></input><br/>
+        <label htmlFor="password">password: </label>
+        <input type="password" name="password"></input><br/>
+        <button name="submitForm" className="submitForm">Register</button>
+      </fieldset>
     );
   }
 }
