aa6bc1f8701396d51d1f47059983dc0a31c5fdd6
Yumemira <surrenderyumi@mail.ru>
2022-11-01 22:54:06 +0300

    Starting do switch method

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import MainMenuElements from './views/MainMenuFrame';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+//love you so much
 
 function App()
 {
-  const elem = MainMenuElements();
-  return elem;
+  return (
+  <Router>
+    <div className="app-container">
+      <Routes>
+        <Route path="/" component={<MainMenu />} />
+      </Routes>
+    </div>
+  </Router>
+  );
+}
+
+class MainMenu extends React.Component
+{
+  render(){
+    const elem = MainMenuElements();
+    return elem;
+  }
 }
 
 export default App;
