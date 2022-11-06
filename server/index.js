6866a3abbb6edf2df4fb473fecc02248d6c395ef
Yumemira <surrenderyumi@mail.ru>
2022-11-06 16:24:10 +0300

    adding client db connect :3

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
+const { Client } = require("pg");
 const express = require("express");
 const cors = require('cors');
 
 const port = process.env.DEFAULT_PORT;
 
 const app = express();
+const client = new Client({
+  user: 'commonmanager',
+  host: 'localhost',
+  database: 'CowBullGameBase',
+  password: 'hant8312',
+  port: 23014,
+});
 
 app.use(cors({origin: process.env.REACT_FRONT_PATH}));
+
+app.post("/register",(req, res, next) => {
+  client.connect()
+  
+});
 
 app.get("/about", (req, res) => {
     res.json({ message: "Love you... forever" });
