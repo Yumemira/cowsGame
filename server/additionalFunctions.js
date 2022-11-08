ec322e6f7940d40ee3b0c0fbc92befe88323ca71
Yumemira <surrenderyumi@mail.ru>
2022-11-08 20:14:31 +0300

    getting message from server too

---
+++ server/additionalFunctions.js
@@ -0,0 +1,30 @@
+const { Client } = require("pg");
+
+module.exports = {
+    queryToDb: function(que)
+    {
+        const client = new Client({
+            user: 'commonmanager',
+            host: 'localhost',
+            database: 'CowBullGameBase',
+            password: 'hant8312',
+            port: 23014
+        });
+
+
+        client.connect();
+
+        return new Promise(function (resolve, reject){
+            client.query(que, (err, res) => {
+                if (err)
+                {
+                    console.error(err);
+                    reject(0);
+                }
+
+                client.end();
+                resolve(res.rows[0]);
+            });
+        });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+const tools = require("./additionalFunctions.js");
 require("dotenv").config();
-const { Client } = require("pg");
+
 const express = require("express");
 const cors = require('cors');
 
 const app = express();
 const port = process.env.DEFAULT_PORT;
+
 
 app.use(express.urlencoded());
 app.use(cors({origin: process.env.REACT_FRONT_PATH}));
@@ -20,36 +22,16 @@ app.post("/register",function(req, res){
   const umail = req.body.umail;
   const upass = req.body.upassword;
 
-  const client = new Client({
-    user: 'commonmanager',
-    host: 'localhost',
-    database: 'CowBullGameBase',
-    password: 'hant8312',
-    port: 23014,
-  });
-
-  const que = `
-    INSERT INTO userstable (name, email, password)
-    VALUES ('`+ uname + `', '`+ umail +`', '`+ upass +`');
-  `;
-  client.connect();
-  client.query(que, (err, res) => {
-    if (err)
-    {
-        console.error(err);
-        return;
-    }
-    console.log('Data insert successful');
-    
-    client.end();
-  
-  });
-
+/*
+  tools.queryToDb(`
+  INSERT INTO userstable (name, email, password)
+  VALUES ('`+ uname + `', '`+ umail +`', '`+ upass +`');
+  `);
+*/
+  tools.queryToDb("select * from userstable")
+  .then((ret) => console.log(ret));
   res.json({message: "data was cathced"});
 });
-
-
-
 
 
 app.listen(port, () => {
