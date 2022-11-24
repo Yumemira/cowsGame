5269b9aeb1372819124bc41fb5deebd3081ff035
Yumemira <surrenderyumi@mail.ru>
2022-11-24 19:29:19 +0300

    get comments from server

--- server/index.js
+++ server/index.js
@@ -3,14 +3,34 @@ require("dotenv").config();
 const tools = require("./additionalFunctions.js");
 const express = require("express");
 const cors = require('cors');
+const http = require('http');
+const { Server } = require("socket.io");
+
 
 const app = express();
 const port = process.env.DEFAULT_PORT;
-var cathcedList = []
 
 app.use(express.urlencoded());
 app.use(cors({origin: process.env.REACT_FRONT_PATH}));
 app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin:"http://localhost:3000",
+    methods: ['GET', 'POST']
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+  
+  socket.on('join_room', (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+  });
+  // We can write our socket event listeners in here...
+});
 
 
 app.get("/pregeneratedId", function(req,res){
@@ -41,6 +61,27 @@ app.post("/posts", function(req, res){
   });
 });
 
+app.post("/comment--send", function(req, res){
+  console.log(req.body);
+  tools.queryToDb(`insert into "Comments" ("postID", data, username, userid)
+    values ($1, $2, $3, $4)`, [ req.body.postId, req.body.textData, req.body.author, req.body.id])
+});
+
+app.post("/comments-fetch", function(req, res){
+  const postId = req.body.postId;
+  tools.queryToDb(`select userid, username, data from "Comments" where "postID" = $1`,[postId])
+  .then((ret) => {
+    if(ret.length > 0)
+    {
+      res.json({message:"success", comList: ret});
+    }
+    else
+    {
+      res.json({message:"empty"});
+    }
+  });
+});
+
 app.post("/register",function(req, res){
   const uname = req.body.uname;
   const umail = req.body.umail;
@@ -55,7 +96,7 @@ app.post("/register",function(req, res){
 
       tools.queryToDb(`
       INSERT INTO userstable (name, email, password, loginkey, state)
-      VALUES ($1,$2,$3,$4, 'o');`, [uname,umail,upass,unicKey]);
+      VALUES ($1,$2,$3,$4, 'o');`, [uname, umail, upass, unicKey]);
 
       tools.queryToDb(`select id from userstable where email = $1 limit 1`,[umail])
       .then(uret =>{
@@ -120,6 +161,6 @@ app.post("/add-new-post", function(req,res){
 });
 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
